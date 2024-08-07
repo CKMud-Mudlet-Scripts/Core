f36fb1c5c1e70bc1ca923a5ac0aac96393fac339
Jason Fried <me@jasonfried.info>
2024-08-06 20:44:36 -0700

    remove chat, fixup motd_bypass with learn, bump version

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ From Time to time the script will issue `score`, and use scouter on yourself to 
 
 ## Installation
 
-`installPackage("https://github.com/CKMud-Mudlet-Scripts/Core/releases/latest/download/CKMud-Core.mpackage")`
+`lua installPackage("https://github.com/CKMud-Mudlet-Scripts/Core/releases/latest/download/CKMud-Core.mpackage")`
 
 After installation `core update` alias will take care of updating the script. 
 
@@ -23,11 +23,15 @@ Type `learn` as soon as possible, this teaches the script what abilities you hav
 ### Keys
 
 F10 - Enable auto fight() (Will automaticlly use best abilities during a fight)
+
 F9 - Start Auto Learn / Training mode using a `child` targets:
 
 ### Aliases
 
 autolearn <target> - Start Auto Learn / Training mode using <target> as attack target. 
+
 configure - Capture android `configure` command so we can save your confiures through vent 
+
 fight <target> - Auto Combo / Melee combat picker 
+
 blast <target> - Auto Ki Attack Picker
--- src/scripts/Configure_Core.lua
+++ src/scripts/Configure_Core.lua
@@ -1,42 +1,30 @@
 local fried = require("CKMud-Core.fried")
 
--- Initalize how we use EMCO
+-- Initalize the used Tables
 registerAnonymousEventHandler(
   "sysLoadEvent",
   fried:run_init(
-    "EMCO Tabs",
+    "Core",
     function()
-      demonnic.chat.consoles = {"All", "OOC", "Tells", "Group", "Auction", "Event", "Say"}
-      demonnic.helpers.save()
+      cecho("<green>[ FRIED ] - Starting __PKGNAME__ v__VERSION__\n")
+      Counters = {}
+      Player = {}
+      Times = {}
+      Timers = {}
+      Toggles = {}
+      PromptCounters = {}
+      PromptFlags = {}
+      Settings = {}
+      Settings.config_attack = "attack 90"
+      -- Auto Learning / Mastery
+      Player.Learned = {}
+      Player.Mastered = {}
+      Player.Boosted = {}
+      Player.Supreme = {}
+      -- Toggle Defaults
+      Toggles.standing = true
+      Toggles.no_fight = false
+      Toggles.wakeok = true
     end
   )
-)
-
--- Initalize the used Tables
-registerAnonymousEventHandler(
-    "sysLoadEvent",
-    fried:run_init(
-        "Core",
-        function()
-            cecho("<green>[ FRIED ] - Starting __PKGNAME__ v__VERSION__\n")
-            Counters = {}
-            Player = {}
-            Times = {}
-            Timers = {}
-            Toggles = {}
-            PromptCounters = {}
-            PromptFlags = {}
-            Settings = {}
-            Settings.config_attack = "attack 90"
-            -- Auto Learning / Mastery
-            Player.Learned = {}
-            Player.Mastered = {}
-            Player.Boosted = {}
-            Player.Supreme = {}
-            -- Toggle Defaults
-            Toggles.standing = true
-            Toggles.no_fight = false
-            Toggles.wakeok = true
-        end
-    )
 )
